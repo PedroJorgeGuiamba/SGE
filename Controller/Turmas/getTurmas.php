d6311a9c675158c9cd01d81e9ebc7854647a6638
Pedro Jorge <pedrojorgeguiamba@gmail.com>
2026-05-08 13:18:20 +0200

    Feature/update-select turmas (#18)
    * Adicionado painel de perfil do user
    
    * Actualizado select de turmas

--- Controller/Turmas/getTurmas.php
+++ Controller/Turmas/getTurmas.php
@@ -3,14 +3,34 @@ require_once __DIR__ . '/../../Conexao/conector.php';
 
 
 $con = new Conector();
-$mysqli = $con->getConexao();
+$conn = $con->getConexao();
 
-$query = "SELECT codigo, nome FROM turma";
-$resultado = mysqli_query($mysqli, $query);
+$termo = trim($_GET['termo'] ?? '');
 
-$options = "<option value=''>Selecione uma Turma</option>";
-while ($row = mysqli_fetch_assoc($resultado)) {
-    $options .= "<option value='{$row['codigo']}'>{$row['nome']}</option>";
+if ($termo === '') {
+    $query = "SELECT codigo, nome FROM turma";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+    // $resultado = mysqli_query($mysqli, $query);
+
+    $options = "<option value=''>Selecione uma Turma</option>";
+    while ($row = $resultado->fetch_assoc()) {
+        $options .= "<option value='{$row['codigo']}'>{$row['nome']}</option>";
+    }
 }
+else{
+    $query = "SELECT codigo, nome FROM turma WHERE codigo_qualificacao = ?";
+    // $resultado = mysqli_query($conn, $query);
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $termo);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
 
+    $options = "<option value=''>Selecione uma Turma</option>";
+    // while ($row = mysqli_fetch_assoc($resultado)) {
+    while ($row = $resultado->fetch_assoc()) {
+        $options .= "<option value='{$row['codigo']}'>{$row['nome']}</option>";
+    }
+}
 echo $options;
